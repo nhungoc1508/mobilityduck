9939f0962d75c929406ab9e6911032f5467b37e1
Hoàng Việt Phương <fcnrt2001@gmail.com>
2025-08-13 14:49:24 +0000

    add docs and span_blob

--- src/include/span.hpp
+++ src/include/span.hpp
@@ -5,15 +5,14 @@
 #include "duckdb/function/scalar_function.hpp"
 #include "duckdb/main/extension_util.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
+#include <tydef.hpp>
 
 extern "C" {
     #include <meos.h>
-    #include <meos_internal.h>    
+    #include <meos_internal.h>
 }
 
 namespace duckdb {
-
-class ExtensionLoader;
 
 struct SpanTypes {
     static const std::vector<LogicalType> &AllTypes();
@@ -29,15 +28,35 @@ struct SpanTypes {
     static void RegisterCastFunctions(DatabaseInstance &instance);
 };
 
+struct SpanFunctions {
+    // for cast
+    static bool Span_to_text(Vector &source, Vector &result, idx_t count, CastParameters &parameters);
+    static bool Text_to_span(Vector &source, Vector &result, idx_t count, CastParameters &parameters);
+    static bool Value_to_span_cast(Vector &source, Vector &result, idx_t count, CastParameters &parameters);
+    static bool Intspan_to_floatspan_cast(Vector &source, Vector &result, idx_t count, CastParameters &parameters);
+    static bool Floatspan_to_intspan_cast(Vector &source, Vector &result, idx_t count, CastParameters &parameters);
+    static bool Datespan_to_tstzspan_cast(Vector &source, Vector &result, idx_t count, CastParameters &parameters);
+    static bool Tstzspan_to_datespan_cast(Vector &source, Vector &result, idx_t count, CastParameters &parameters);
+    // scalar functions
+    static void Span_as_text(DataChunk &args, ExpressionState &state, Vector &result);    
+    static void Span_constructor(DataChunk &args, ExpressionState &state, Vector &result);   
+    static void Value_to_span(DataChunk &args, ExpressionState &state, Vector &result);
+    static void Intspan_to_floatspan(DataChunk &args, ExpressionState &state, Vector &result);
+    static void Floatspan_to_intspan(DataChunk &args, ExpressionState &state, Vector &result);
+    static void Datespan_to_tstzspan(DataChunk &args, ExpressionState &state, Vector &result);
+    static void Tstzspan_to_datespan(DataChunk &args, ExpressionState &state, Vector &result);    
+    static void Span_mem_size(DataChunk &args, ExpressionState &state, Vector &result);
+    static void Span_num_values(DataChunk &args, ExpressionState &state, Vector &result);
+    static void Span_start_value(DataChunk &args, ExpressionState &state, Vector &result);
+    static void Span_end_value(DataChunk &args, ExpressionState &state, Vector &result);
+    static void Span_value_n(DataChunk &args, ExpressionState &state, Vector &result_vec);
+    static void Span_values(DataChunk &args, ExpressionState &state, Vector &result);
+    static void Numspan_shift(DataChunk &args, ExpressionState &state, Vector &result);
+};
+
 struct SpanTypeMapping {
     static meosType GetMeosTypeFromAlias(const std::string &alias);
     static LogicalType GetChildType(const LogicalType &type);
-};
-
-// Add this new struct for cast functions
-struct SpanFunctions {
-    static bool StringToSpan(Vector &source, Vector &result, idx_t count, CastParameters &parameters);
-    static bool SpanToString(Vector &source, Vector &result, idx_t count, CastParameters &parameters);
 };
 
 } // namespace duckdb
