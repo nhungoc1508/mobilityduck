706f09c7609f202aad81dcf13f9be1ea75240da6
Ngoc Hoang <nnh245@nyu.edu>
2025-10-11 19:18:12 +0000

    Updated main extension file

--- src/mobilityduck_extension.cpp
+++ src/mobilityduck_extension.cpp
@@ -81,20 +81,12 @@ static void ConfigureMeosSridCsvOnce() {
     });
 }
 
-static void geos_message_handler(const char* fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    vprintf(fmt, ap);
-    va_end(ap);
-}
-
 static void LoadInternal(DatabaseInstance &instance) {
 	ConfigureMeosSridCsvOnce();
 	// Initialize MEOS
 	static std::once_flag meos_init_flag;
     std::call_once(meos_init_flag, []() {
         meos_initialize();
-        initGEOS(geos_message_handler, geos_message_handler);
     });
 
 	Connection con(instance);
